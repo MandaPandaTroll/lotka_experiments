e9e8f6e56e3f96a76f03e00dafe7454b3cdaec95
MandaPandaTroll <viddadodda@gmail.com>
2022-05-04 11:00:12 +0200

    Adding the script.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # popMathematicalModels
-Mathematical models of population dynamics.
 
-This is a line from RStudio.
+A population dynamics model built upon the lotka-volterra equations, with the addition of stochasticity from a normal distribution, a third trophic level, density dependent growth for autotrophs and a few more constants to modulate the interactions between the primary and secondary consumers.
+
+tabacwoman May 2022
+
+CONSTANTS
+ a = intrinsic growth rate (prey)
+ b = predation and other population loss coefficient (prey)
+ k = density dependent growth coefficient (prey)
+
+ c = other population loss coefficient (predator)
+ d = intrinsic growth rate (predator)
+ q = predation coefficient (predator)
+
+ m = intrinsic growth rate (apex predator)
+ w = population loss coefficient (apex predator)
+
+ VARIABLES
+ x = prey population
+ y = predator population
+ z = apex predator population
---
+++ lotk1.R
@@ -0,0 +1,108 @@
+#tabacwoman May 2022
+
+
+rm(list=ls())
+
+
+library(ggplot2)
+library(tidyverse)
+
+
+
+
+
+#CONSTANTS
+# a = intrinsic growth rate (prey)
+# b = predation and other population loss coefficient (prey)
+# k = density dependent growth coefficient (prey)
+
+# c = other population loss coefficient (predator)
+# d = intrinsic growth rate (predator)
+# q = predation coefficient (predator)
+
+# m = intrinsic growth rate (apex predator)
+# w = population loss coefficient (apex predator)
+
+# VARIABLES
+# x = prey population
+# y = predator population
+# z = apex predator population
+
+{
+  preyEq <- function(a, b, k, x, y, dt) return(x+ (a*x - (b*x*y + k*(x^2))/dt))
+  predEq <- function(d, c, q, x, y, z, dt) return(y+ (d*x*y -( c*y +q*z))/dt)
+  apexEq <- function(m, w, z, y, dt) return(z+ (m*z*y - w*z)/dt)
+  
+  
+  stochast <- unlist(lapply( rnorm(10000,mean = 0, sd = 0.5), round),use.names = FALSE)  
+  
+  
+cycles <- 5e4 # number of time steps
+
+#initial population sizes
+x <- 500
+y <- 200
+z <- 50
+
+Pops <- data.frame(prey = c(x,x,x), predator = c(y,y,y), apex = c(z,z,z))
+
+Pops[0,1] <- x
+Pops[1,1] <- x
+Pops[0,2] <- y
+Pops[1,2] <- y
+Pops[0,3] <- z
+Pops[1,3] <- z
+}
+
+
+for (i in 2:cycles){
+  
+  if(Pops[i-1,1] > 0){x <- Pops[i-1,1]}else{x <- 0}
+  if(Pops[i-1,2] > 0){y <- Pops[i-1,2]}else{y <- 0}
+  if(Pops[i-1,3] > 0){z <- Pops[i-1,3]}else{z <- 0}
+               
+  if(x < 0){x <- 0}
+  if(y < 0){y <- 0} 
+  if(z < 0){z <- 0}
+    
+    Pops[i,1] <-  preyEq(1e-2, 3e-5, 1e-5, x, y,1.0 ) +sample(stochast,1)
+    Pops[i,2] <-  predEq(7e-7, 2e-4, 1.5e-4, x, y, z, 1.0 ) +sample(stochast,1)
+    Pops[i,3] <-  apexEq(5e-6, 8e-4, z, y,1.0 ) +sample(stochast,1)
+    
+    
+    
+    
+    }
+  
+
+#Plot of population sizes
+{
+  Pops_gathered <- Pops%>%mutate(t = seq(1:length(Pops$prey)))
+
+Pops_gathered <- Pops_gathered %>%
+  select(t, prey, predator, apex) %>%
+  gather(key = "species", value = "individuals", -t)
+
+
+ggplot(Pops_gathered, aes(x = t, y = individuals)) + 
+  geom_line(aes(color = species))+
+  scale_color_manual(values = c("red", "blue","forestgreen"))+theme_bw()}
+
+
+#scaled plot of population sizes [-1,1], can be used to convert the data into audio files.
+{
+normpops <- Pops
+normpops$prey <- 2*normpops$prey/max(normpops$prey) -1
+normpops$predator <- 2*normpops$predator/max(normpops$predator) -1
+normpops$apex <- 2*normpops$apex/max(normpops$apex) -1
+
+normpops_gathered <- normpops%>%mutate(t = seq(1:length(normpops$prey)))
+
+normpops_gathered <- normpops_gathered %>%
+  select(t, prey, predator, apex) %>%
+  gather(key = "species", value = "individuals", -t)
+
+
+ggplot(normpops_gathered, aes(x = t, y = individuals)) + 
+  geom_line(aes(color = species))+
+  scale_color_manual(values = c("red", "blue","forestgreen"))+theme_bw()}
